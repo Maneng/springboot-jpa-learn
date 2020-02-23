6dcb47dbaedbeb4e8de10cd28db856e216933bc1
maneng <nengma@clubfactory.com>
2020-02-23 17:37:07 +0800

    updaate

--- src/main/java/com/maneng/jpa/demo/entity/Order.java
+++ src/main/java/com/maneng/jpa/demo/entity/Order.java
@@ -1,8 +1,18 @@
 package com.maneng.jpa.demo.entity;
 
+import com.maneng.jpa.demo.foundation.persistence.LongTimestampedEntity;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author maneng
  * @date created at 2020/2/23 5:18 下午
  */
-public class Order {
+@Data
+@Entity
+@Table(name = "order")
+public class Order extends LongTimestampedEntity {
+
 }
